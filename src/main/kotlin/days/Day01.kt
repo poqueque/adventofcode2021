7f1c61cc7550c1cc5c08f3faa32e0b321dc9fb21
Edi <poqueque@yahoo.es>
2021-12-01 07:12:41 +0100

    Day 01

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -3,10 +3,32 @@ package days
 class Day01 : Day(1) {
 
     override fun partOne(): Any {
-        return 0
+        var previous = 0
+        var increases = 0
+        for (measure in inputList) {
+            val m = measure.toInt()
+            if (m > previous) increases++
+            previous = m
+        }
+        return increases
     }
 
     override fun partTwo(): Any {
-        return 0
+        var previous = mutableListOf<Int>()
+        var prevVal = 1000000
+        var increases = 0
+        for (measure in inputList) {
+            val m = measure.toInt()
+            previous.add(m)
+            while (previous.size > 3) {
+                previous = previous.drop(1).toMutableList()
+            }
+            if (previous.size == 3) {
+                val total = previous.sum()
+                if (total > prevVal) increases++
+                prevVal = total
+            }
+        }
+        return increases
     }
 }
